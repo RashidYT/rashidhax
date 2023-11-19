1ea6a97e1f2d76e3e959cbfffef7514b925adbc0
RashidYT <80108767+RashidYT@users.noreply.github.com>
2023-11-19 22:14:20 +0300

    dgfh ggd
    fgdsfgsdfgsdfgd

---
+++ fun/dinosonic.js
@@ -0,0 +1,4 @@
+document.getElementById("offline-resources-1x").src = " https://i.imgur.com/7BuRkS9.png";
+document.getElementById("offline-resources-2x").src = " https://i.imgur.com/7BuRkS9.png";
+Runner.instance_.setSpeed(1000)
+Runner.instance_.distanceRan = 100
